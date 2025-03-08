13b69892f86ffe91fd330257601ff7314d2663b3
zekaran30 <zekaran30@gmail.com>
2025-03-08 15:30:57 -0500

    implemented springboot

---
+++ Zalik/src/main/java/udemhacksZalik/Zalik/ZalikApplication.java
@@ -0,0 +1,13 @@
+package udemhacksZalik.Zalik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZalikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZalikApplication.class, args);
+	}
+
+}
---
+++ Zalik/src/test/java/udemhacksZalik/Zalik/ZalikApplicationTests.java
@@ -0,0 +1,13 @@
+package udemhacksZalik.Zalik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZalikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
