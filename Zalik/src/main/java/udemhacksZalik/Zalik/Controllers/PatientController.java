d0feed7bf087e279795daa3304167fefae31a9ec
Roshnikarwal <roshnikarwal285@gmail.com>
2025-03-09 09:19:28 -0400

    Idk commiting to get the changes Patience service waws added shoutout Leo fr

--- Zalik/src/main/java/udemhacksZalik/Zalik/Controllers/PatientController.java
+++ Zalik/src/main/java/udemhacksZalik/Zalik/Controllers/PatientController.java
@@ -1,20 +1,28 @@
 package udemhacksZalik.Zalik.Controllers;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.sqlite.JDBC;
+import udemhacksZalik.Zalik.Services.PatientService;
 import udemhacksZalik.Zalik.models.Patient;
 import java.util.List;
 import udemhacksZalik.Zalik.models.UserService;
 
+@Controller
 public class PatientController {
-    private UserService userService;
+    private final PatientService patientService;
 
-    public PatientController(JdbcTemplate bob) {
-        this.userService = new UserService(bob);
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
     }
 
-    public List<Patient> getPatients() {
-        String sql = "SELECT * FROM patients";
-        return userService.jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Patient.class));
+    @GetMapping("/view-patients")
+    public String getPatients(Model model) {
+        List<Patient> patients = patientService.getAllPatients();
+        model.addAttribute("patients", patients);
+        return "view-patients-list";
     }
 }
