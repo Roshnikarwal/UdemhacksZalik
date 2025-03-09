08982f28df502c6a154d5841a8f7127617faf737
zekaran30 <zekaran30@gmail.com>
2025-03-08 22:52:49 -0500

    created homepage and styled

--- Zalik/src/main/java/udemhacksZalik/Zalik/Controllers/HomeController.java
+++ Zalik/src/main/java/udemhacksZalik/Zalik/Controllers/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "index"; // Refers to src/main/resources/templates/index.html
+        return "home"; // Refers to src/main/resources/templates/index.html
     }
 }
