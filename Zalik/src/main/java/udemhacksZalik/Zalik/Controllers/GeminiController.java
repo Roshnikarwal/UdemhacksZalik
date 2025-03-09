7f0f95d2eb60860b64902b0e8638b5d62d811657
zekaran30 <zekaran30@gmail.com>
2025-03-09 07:58:53 -0400

    the chatbot works

--- Zalik/src/main/java/udemhacksZalik/Zalik/Controllers/GeminiController.java
+++ Zalik/src/main/java/udemhacksZalik/Zalik/Controllers/GeminiController.java
@@ -2,21 +2,18 @@ package udemhacksZalik.Zalik.Controllers;
 
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 
 @RestController
 @RequestMapping("/api/gemini")
+@CrossOrigin(origins = "*")
 public class GeminiController {
     @Autowired
     private VertexAiGeminiChatModel chatModel;
 
     public GeminiController(VertexAiGeminiChatModel chatModel) {
         this.chatModel = chatModel;
-        System.out.println("THIS IS THE CACA PIPI THAT IS CACA PIPIYING "+chatModel.toString());
     }
 
     @GetMapping("/chat")
